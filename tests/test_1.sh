1c0d748adaf4ac40d85e70ab24923ac2b9ee36d9
Ian Kirker <ian.kirker@gmail.com>
2018-05-17 13:15:27 +0100

    Adds a test file instead of trying to generate one

--- tests/test_1.sh
+++ tests/test_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo --non-interactive --user=user_1 bash <<<"sha256sum >~/test_file <<<$RANDOM"
+sudo --non-interactive --user=user_1 cp "$TEST_DIR/test_data" ~user_1/
 sudo --non-interactive --user=user_1 "$PARENT_DIR/give" user_2 ~user_1/test_file
 sudo --non-interactive --user=user_2 "$PARENT_DIR/take" user_1 test_file
 
