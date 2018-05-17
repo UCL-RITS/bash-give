85e0f91a2890aa77149f7024aeb75c1a12afc200
Ian Kirker <ian.kirker@gmail.com>
2018-05-17 12:34:43 +0100

    Clean up file removal, add error backtrace funcs

--- tests/all_tests.sh
+++ tests/all_tests.sh
@@ -12,6 +12,33 @@ export PATH="$PARENT_DIR:$PATH"
 
 export GIFT_STORE="/gift-store"
 
+#===================================
+# Defs for better error reporting
+#===================================
+
+function error() {
+  echo "Error: $*" >&2
+  [[ "${debug_mode:-0}" -eq 1 ]] && callstack
+  exit 1
+}
+
+function callstack() {
+    echo --- Trace: ---
+    local i
+    # These indexes start at 0 but 0 is this callstack function, so skip it.
+    for ((i=1; i<${#FUNCNAME[*]}; i++)) do
+        if [ $i -ne 1 ]; then
+            echo -n "  "  
+        fi
+        echo "${BASH_SOURCE[$i]}: in \"${FUNCNAME[$i]}\" called from line ${BASH_LINENO[$i]}"
+    done
+    echo --------------
+}
+
+[[ "$debug_mode:-0}" -eq 1 ]] && trap callstack ERR
+
+#==================================
+
 "$TESTS_DIR/setup.sh"
 
 "$TESTS_DIR/test_1.sh"
--- tests/setup.sh
+++ tests/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source settings.sh
-
 for loop_user in user_1 user_2 user_3; do
     sudo useradd --create-home --user-group $loop_user
 done
