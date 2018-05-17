ea4d53ed243e8b499136621e5ebe10d5c125ef56
Ian Kirker <ian.kirker@gmail.com>
2018-05-17 15:16:18 +0100

    Corrects test_1 and adds final equality check

--- tests/test_1.sh
+++ tests/test_1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo --non-interactive --user=user_1 cp "$TESTS_DIR/test_data" ~user_1/
-sudo --non-interactive --user=user_1 "$PARENT_DIR/give" user_2 ~user_1/test_file
-sudo --non-interactive --user=user_2 "$PARENT_DIR/take" user_1 test_file
+sudo --non-interactive --user=user_1 "$PARENT_DIR/give" user_2 ~user_1/test_data
+sudo --non-interactive --user=user_2 bash -c "cd ~user_2; \"$PARENT_DIR/take\" user_1 test_data"
+
+sudo --non-interactive diff ~user_1/test_data ~user_2/test_data
 
