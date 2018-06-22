ca90e0aefe6dfa697f9c0ca5f2bd3ac4584794af
Ian Kirker <ian.kirker@gmail.com>
2018-06-22 12:16:48 +0100

    Setting GIFT_STORE for sudo-d gives in test_2

--- tests/test_2.sh
+++ tests/test_2.sh
@@ -5,5 +5,5 @@
 sudo --non-interactive --user=user_3 mkdir -p "$GIFT_STORE"/user_1/user_2
 
 sudo --non-interactive --user=user_1 cp "$TESTS_DIR/test_data" ~user_1/
-! sudo --non-interactive --user=user_1 "$PARENT_DIR/give" user_2 ~user_1/test_data
-! sudo --non-interactive --user=user_2 bash -c "cd ~user_2; \"$PARENT_DIR/take\" user_1 test_data"
+! sudo --non-interactive --user=user_1 GIFT_STORE="$GIFT_STORE" "$PARENT_DIR/give" user_2 ~user_1/test_data
+! sudo --non-interactive --user=user_2 bash -c "export GIFT_STORE=\"$GIFT_STORE\"; cd ~user_2; \"$PARENT_DIR/take\" user_1 test_data"
