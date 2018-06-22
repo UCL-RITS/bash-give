ea79febf8fc3d783c449a70831f5431c69187e59
Ian Kirker <ian.kirker@gmail.com>
2018-06-22 11:12:36 +0100

    Adds test_2 for incorrect giftstore ownership

--- tests/all_tests.sh
+++ tests/all_tests.sh
@@ -44,3 +44,4 @@ function callstack() {
 "$TESTS_DIR/setup.sh"
 
 "$TESTS_DIR/test_1.sh"
+"$TESTS_DIR/test_2.sh"
---
+++ tests/test_2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Tests for failure when gift dir for user_1 is owned by !user_1
+
+sudo --non-interactive --user=user_3 mkdir "$GIFT_STORE"/user_1/user_2
+
+sudo --non-interactive --user=user_1 cp "$TESTS_DIR/test_data" ~user_1/
+! sudo --non-interactive --user=user_1 "$PARENT_DIR/give" user_2 ~user_1/test_data
+! sudo --non-interactive --user=user_2 bash -c "cd ~user_2; \"$PARENT_DIR/take\" user_1 test_data"
