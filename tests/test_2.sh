3b805097161d82a2792d614afc929ae6c6db63e6
Ian Kirker <ian.kirker@gmail.com>
2018-06-22 12:28:12 +0100

    Corrected correction of fake gift dir

--- tests/test_2.sh
+++ tests/test_2.sh
@@ -2,7 +2,7 @@
 
 # Tests for failure when gift dir for user_1 is owned by !user_1
 
-sudo --non-interactive --user=user_3 mkdir -p "$GIFT_STORE"/"$(id -u user_1)"/"$(id -u user_2)"
+sudo --non-interactive --user=user_3 mkdir -p "$GIFT_STORE"/"$(id -u user_1)"-"$(id -u user_2)"
 
 sudo --non-interactive --user=user_1 cp "$TESTS_DIR/test_data" ~user_1/
 ! sudo --non-interactive --user=user_1 GIFT_STORE="$GIFT_STORE" "$PARENT_DIR/give" user_2 ~user_1/test_data
