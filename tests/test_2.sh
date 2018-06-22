881a84cdea2c96ac93d53e2b54d6770ade1ada46
Ian Kirker <ian.kirker@gmail.com>
2018-06-22 11:16:09 +0100

    Bugfix in test_2.sh

--- tests/test_2.sh
+++ tests/test_2.sh
@@ -2,7 +2,7 @@
 
 # Tests for failure when gift dir for user_1 is owned by !user_1
 
-sudo --non-interactive --user=user_3 mkdir "$GIFT_STORE"/user_1/user_2
+sudo --non-interactive --user=user_3 mkdir -p "$GIFT_STORE"/user_1/user_2
 
 sudo --non-interactive --user=user_1 cp "$TESTS_DIR/test_data" ~user_1/
 ! sudo --non-interactive --user=user_1 "$PARENT_DIR/give" user_2 ~user_1/test_data
